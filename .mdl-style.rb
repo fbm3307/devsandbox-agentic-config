39844e75fe5e3c1c1fa87cd2cd1e7695b1e8eeb5
MikelAlejoBR <mbarcina@redhat.com>
2026-03-30 23:10:50 -0400

    feature: gracefully deal with Markdown frontmatters
    Some files might contain frontmatters, which should not invalidate the
    Markdown files. This commit adjusts the linter to gracefully deal with
    them.

--- .mdl-style.rb
+++ .mdl-style.rb
@@ -1,5 +1,7 @@
 all
-# Ignore the maximum line length rule for code blocks.
+# MD002 (first header should be a top level header) is superseded by MD041.
+exclude_rule 'MD002'
+# Ignore the maximum line length rule for code blocks and tables.
 rule 'MD013', :ignore_code_blocks => true, :tables => false
 # Lists must use the indexes in ascending order.
 rule 'MD029', :style => :ordered
